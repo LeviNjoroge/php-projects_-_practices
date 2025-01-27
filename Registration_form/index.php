078d53b8e2d5494f4c43242456cd2f8d01b28a41
Levi Njoroge <levynjoroge1@gmail.com>
2025-01-27 16:00:38 +0300

    workking on php...

--- Registration_form/index.php
+++ Registration_form/index.php
@@ -1,5 +1,15 @@
 <?php
-
+    $conn = new mysqli("localhost", "root", "", "Test1_Reg");
+    $FName = $_POST['FName'];
+    $LName = $_POST['LName'];
+    $gender = $_POST['gender'] ?? null; // Use null coalescing to handle missing key
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $stmt = $conn->prepare("INSERT INTO Users (FName, LName, gender, email, password) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $FName, $LName, $gender, $email, $password);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
 ?>
 
 <!DOCTYPE html>
@@ -16,27 +26,27 @@
         <form action="" method="post">
             <div id="formElements">
                 <label for="FName">First Name: </label> <br>
-                <input type="text" name="FName" id="FName" placeholder="Joe" required> <br>
+                <input type="text" name="FName" id="FName" placeholder="Joe" required style="width: 50%;"> <br>
             </div>
             <div id="formElements">
                 <label for="LName">Last Name: </label> <br>
-                <input type="text" name="LName" id="LName" placeholder="Doe" required> <br>
+                <input type="text" name="LName" id="LName" placeholder="Doe" required style="width: 50%;"> <br>
             </div>
             <div id="formElements">
                 <label for="gender">Gender:</label> <br>
                 <div>
-                    <label for="male">Male</label> <input type="radio" name="gender" id="male" value="M">
-                    <label for="female">Female</label> <input type="radio" name="gender" id="female" value="F">
-                    <label for="other">Other</label> <input type="radio" name="gender" id="other" value="O">
+                    <input type="radio" name="gender" id="male" value="M"> Male
+                    <input type="radio" name="gender" id="female" value="F"> Female
+                    <input type="radio" name="gender" id="other" value="O"> Other
                 </div>
             </div>
             <div id="formElements">
                 <label for="email">Email:</label> <br>
-                <input type="email" name="email" id="email" placeholder="joedoe@example.com" required> <br>
+                <input type="email" name="email" id="email" placeholder="joedoe@example.com" required style="width: 50%;"> <br>
             </div>
             <div id="formElements">
                 <label for="password">Password:</label> <br>
-                <input type="password" name="password" id="password" password placeholder="********" required> <br> <br>
+                <input type="password" name="password" id="password" password placeholder="********" required style="width: 50%;"> <br> <br>
             </div>
             <input type="submit" value="Submit" class="submitButton">
             <footer style="margin-right: 10px; text-align: right;">
